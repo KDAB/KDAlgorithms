aafa51f75fed53cb51374159959dde46f9c2d9ca
Jesper K. Pedersen <jesper.pedersen@kdab.com>
2023-06-19 10:38:47 +0200

    Call std::invoke if we have it

--- src/kdalgorithms_bits/invoke.h
+++ src/kdalgorithms_bits/invoke.h
@@ -13,46 +13,48 @@
 
 namespace kdalgorithms {
 namespace detail {
+#if __cplusplus < 201703L
     template <typename Function, typename Object, typename DontCare1, typename DontCare2,
               typename... Args>
     decltype(auto) invoke_helper(Function function, Object &&object,
-                                 std::false_type /* is member pointer*/, DontCare1, DontCare2,
-                                 Args &&...args)
+                                 std::false_type /* is_member_pointer<Function> */, DontCare1,
+                                 DontCare2, Args &&...args)
     {
         return function(object, std::forward<Args>(args)...);
     }
 
     template <typename Member, typename Object, typename... Args>
     decltype(auto) invoke_helper(Member member, Object &&object,
-                                 std::true_type /* is member pointer */,
-                                 std::true_type /* is member function */,
-                                 std::true_type /* is object a pointer */, Args &&...args)
+                                 std::true_type /* is_member_pointer<Function> */,
+                                 std::true_type /* is_member_function_pointer<Function> */,
+                                 std::true_type /* is_pointer<Type> */, Args &&...args)
     {
         return (std::forward<Object>(object)->*member)(std::forward<Args>(args)...);
     }
 
     template <typename Member, typename Object>
-    decltype(auto)
-    invoke_helper(Member member, Object &&object, std::true_type /* is member pointer */,
-                  std::false_type /* is member function */, std::true_type /* is object a pointer*/)
+    decltype(auto) invoke_helper(Member member, Object &&object,
+                                 std::true_type /* is_member_pointer<Function> */,
+                                 std::false_type /* is_member_function_pointer<Function> */,
+                                 std::true_type /* is_pointer<Type> */)
     {
         return std::forward<Object>(object)->*member;
     }
 
     template <typename Member, typename Object, typename... Args>
     decltype(auto) invoke_helper(Member member, Object &&object,
-                                 std::true_type /* is member pointer */,
-                                 std::true_type /* is member function */,
-                                 std::false_type /* is object a pointer */, Args &&...args)
+                                 std::true_type /* is_member_pointer<Function> */,
+                                 std::true_type /* is_member_function_pointer<Function> */,
+                                 std::false_type /* is_pointer<Type> */, Args &&...args)
     {
         return (std::forward<Object>(object).*member)(std::forward<Args>(args)...);
     }
 
     template <typename Member, typename Object>
     decltype(auto) invoke_helper(Member member, Object &&object,
-                                 std::true_type /* is member pointer */,
-                                 std::false_type /* is member function */,
-                                 std::false_type /* is object a pointer */)
+                                 std::true_type /* is_member_pointer<Function> */,
+                                 std::false_type /* is_member_function_pointer<Function> */,
+                                 std::false_type /* is_pointer<Type> */)
     {
         return std::forward<Object>(object).*member;
     }
@@ -66,6 +68,13 @@ namespace detail {
                              std::is_member_function_pointer<Function>(), std::is_pointer<Type>(),
                              std::forward<Args>(args)...);
     }
+#else
+    template <typename Function, typename Object, typename... Args>
+    decltype(auto) invoke(Function function, Object &&object, Args &&...args)
+    {
+        return std::invoke(function, std::forward<Object>(object), std::forward<Args>(args)...);
+    }
+#endif
 
 } // namespace detail
 } // kdalgorithms
